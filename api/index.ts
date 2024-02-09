6c558c10ee48b47c1bfedc7104a6babd2cc636d4
Edil Hudaiberdiev <hudaiberdiev.ed@gmail.com>
2024-02-10 02:30:59 +0600

    add artist entity get & post

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import mongoose from 'mongoose';
-import config from './config';
-
 const cors = require('cors');
+import mongoose from 'mongoose';
+
+import config from './config';
+import artistsRouter from "./routers/artists";
 
 const app = express();
 const port = 8000;
@@ -10,13 +11,14 @@ const port = 8000;
 app.use(express.json());
 app.use(cors());
 
+app.use('/artists', artistsRouter);
+
 const run = async () => {
     await mongoose.connect(config.mongoose.db);
 
     app.listen(port, () => {
         console.log(`Server started on ${port} port!`);
     });
-
 
     process.on('exit', () => {
         mongoose.disconnect();
---
+++ api/routers/artists.ts
@@ -0,0 +1,38 @@
+import {Router} from 'express';
+import {imagesUpload} from "../multer";
+import mongoose from "mongoose";
+import Artist from "../models/Artist";
+
+const artistsRouter = Router();
+
+artistsRouter.post('/', imagesUpload.single('image'), async (req, res, next) => {
+    try {
+        const artistData = {
+            title: req.body.title,
+            image: req.file ? req.file.filename : null,
+            description: req.body.description,
+        };
+
+       const artist = new Artist(artistData);
+       await artist.save();
+
+       res.send(artist);
+    } catch (e) {
+        if (e instanceof mongoose.Error.ValidationError) {
+            return res.status(422).send(e);
+        }
+
+        next(e);
+    }
+});
+
+artistsRouter.get('/', async (req, res, next) => {
+    try {
+        const artist = await Artist.find();
+        return res.send(artist);
+    } catch (e) {
+        next(e);
+    }
+});
+
+export default artistsRouter
