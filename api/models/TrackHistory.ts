8d9d632eb3ec74acf043c5e64a3fa6d802a01367
Edil Hudaiberdiev <hudaiberdiev.ed@gmail.com>
2024-02-15 01:29:18 +0600

    started trachhistory token

--- api/models/TrackHistory.ts
+++ api/models/TrackHistory.ts
@@ -1,6 +1,7 @@
 import mongoose, {Schema, Types} from 'mongoose';
 import User from "./User";
 import Track from "./Track";
+import {randomUUID} from "crypto";
 
 const TrackHistorySchema = new mongoose.Schema({
     user: {
@@ -32,7 +33,16 @@ const TrackHistorySchema = new mongoose.Schema({
         type: Date,
         default: () => new Date(),
     },
+
+    token: {
+        type: String,
+        required: true,
+    }
 });
+
+TrackHistorySchema.methods.generateToken = function () {
+    this.token = randomUUID();
+}
 
 const TrackHistory = mongoose.model('TrackHistory', TrackHistorySchema);
 
--- api/routers/trackHistoryRouter.ts
+++ api/routers/trackHistoryRouter.ts
@@ -1,6 +1,9 @@
 import {Router} from 'express';
 import mongoose from "mongoose";
 import TrackHistory from "../models/TrackHistory";
+import Album from "../models/Album";
+import User from "../models/User";
+import Track from "../models/Track";
 
 const trackHistoryRouter = Router();
 
@@ -14,6 +17,7 @@ trackHistoryRouter.post('/', async (req, res, next) => {
         };
 
         const trackHistory = new TrackHistory(trackHistoryData);
+        trackHistory.generateToken();
         await trackHistory.save();
 
         res.send(trackHistory);
@@ -29,6 +33,16 @@ trackHistoryRouter.post('/', async (req, res, next) => {
 trackHistoryRouter.get('/', async (req, res, next) => {
     try {
         const trackHistory = await TrackHistory.find();
+
+        let user_id = req.body.user
+        let track_id = req.body.track
+
+        if (req.body.user) {
+            req.body.user =  await User.findOne({user_id});
+        } if (req.body.track) {
+            req.body.track=  await Track.findOne({track_id});
+        }
+
         return res.send(trackHistory);
     } catch (e) {
         next(e);
