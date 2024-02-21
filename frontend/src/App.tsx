5707ebf85aed5923de1f48fa5e8959709d3f0f2a
Edil Hudaiberdiev <hudaiberdiev.ed@gmail.com>
2024-02-22 02:04:09 +0600

    hw-85 Frontend app init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css';
+import {Route, Routes} from 'react-router-dom';
+import Home from './Containers/Home/Home';
+import Toolbar from './Components/UI/Toolbar/Toolbar';
+
+const App = () => {
+
+  return (
+    <>
+      <header>
+        <Toolbar/>
+      </header>
+      <main className="mt-5">
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="*" element={(<h1>Not found</h1>)}/>
+        </Routes>
+      </main>
+
+    </>
+  );
+};
+
+export default App;
---
+++ frontend/src/Containers/Home/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+
+  return (
+    <div className="container">
+        Future music app
+    </div>
+  );
+};
+
+export default Home;
